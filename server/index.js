75ba0c039640e3fc779f3e1b964d0552b1eb5990
Haris <hnm@opinionai.ai>
2024-08-04 01:45:58 +0500

    fixes the server port bind

--- server/index.js
+++ server/index.js
@@ -89,8 +89,8 @@ app.get('/keep-alive', (req, res) => {
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
   console.log('MongoDB connected');
-  app.listen(5000, function () {
-    console.log('Server is running on port 5000');
+  app.listen(process.env.PORT, function () {
+    console.log(`Server is running on port ${process.env.PORT}`);
   });
 
   // Schedule the self-pinging task
